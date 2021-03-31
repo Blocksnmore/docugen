218330a2229619d4374c95dc712fcf257c153edb
Blocksnmore <51055767+Blocksnmore@users.noreply.github.com>
2021-03-31 10:32:22 -0500

    Initial commit

---
+++ compiler/index.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const marked = require("marked");
+const sanitizehtml = require("sanitize-html");
+console.log("Starting DocuGen Version " + require("../package.json").version);
+var docjson = [];
+
+if (fs.existsSync("." + require("../config.json").output + "/"))
+  fs.rmdirSync("." + require("../config.json").output + "/", {
+    recursive: true,
+  });
+if (!fs.existsSync("." + require("../config.json").input + "/")) {
+  console.log("MARKDOWN FOLDER NOT MADE! Creating");
+  fs.mkdirSync("." + require("../config.json").input + "/");
+}
+
+docugen("");
+
+async function docugen(directory) {
+  fs.readdir(
+    "." + require("../config.json").input + "/" + directory,
+    async (err, file) => {
+      if (!file || file.length < 1) return;
+      let dirs = file.filter((f) => !isFile(directory + "/" + f));
+      let files = file.filter((f) => isFile(directory + "/" + f));
+      dirs.forEach((f) => {
+        docugen(f);
+      });
+      files.forEach(async (f) => {
+        let md = await marked(
+          fs.readFileSync(
+            "." + require("../config.json").input + "/" + directory + "/" + f,
+            "utf-8"
+          )
+        );
+        md = await sanitizehtml(md);
+        let dir =
+          "." +
+          require("../config.json").output +
+          "/" +
+          directory +
+          (directory === "" ? "" : "/") +
+          (f.toLowerCase().endsWith(".md")
+            ? f.toLowerCase() ===
+              require("../config.json").main.toLowerCase() + ".md"
+              ? "index.html"
+              : f.toString().substring(0, f.toString().length - 3) + ".html"
+            : f.toString());
+        fs.mkdirSync(
+          "." +
+            require("../config.json").output +
+            "/" +
+            directory +
+            (directory === "" ? "" : "/")
+        );
+        fs.writeFileSync(dir, md);
+      });
+    }
+  );
+}
+function isFile(file) {
+  return !fs
+    .lstatSync("." + require("../config.json").input + "/" + file)
+    .isDirectory();
+}
---
+++ docs/home.md
@@ -0,0 +1,3 @@
+# How to use DocuGen
+To use docugen create all of your md files and folders, once created go to `config.json` and configure it. <br>
+When everything is configured run `node .` and the compiler will create an HTML version in your output directory (You don't need to create a folder)
