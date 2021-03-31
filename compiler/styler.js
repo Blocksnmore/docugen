9b169ac0db9a8f8566abf13c27b447a661455452
Blocksnmore <51055767+Blocksnmore@users.noreply.github.com>
2021-03-31 11:12:58 -0500

    code

--- compiler/styler.js
+++ compiler/styler.js
@@ -1,6 +1,6 @@
 module.exports = class {
   constructor(md) {
     md = '<article class="markdown-body">' + md + "</article>";
-    this.md = '<link rel="stylesheet" href="/styles.css">' + md.split("\n").join("");
+    this.md = '<link rel="stylesheet" href="'+require("../config.json").cssurl+'">' + md.split("\n").join("");
   }
 };
